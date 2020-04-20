d03d7b68a0b4f56f227b8bc01605d852748c15a8
Pierce Darragh <pierce.darragh@gmail.com>
2020-04-20 13:46:25 -0600

    Enable RGB_MATRIX and begin custom implementation of RGB effects

--- keyboards/massdrop/alt/keymaps/pdarragh/rules.mk
+++ keyboards/massdrop/alt/keymaps/pdarragh/rules.mk
@@ -1,3 +1,7 @@
 # This keymap requires Massdrop Configurator support
 OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
 TAP_DANCE_ENABLE = yes
+
+# Enable custom RGB matrix effects.
+RGB_MATRIX_ENABLE = custom
+RGB_MATRIX_CUSTOM_USER = yes
